8e2dcefdfe6f6cbdf5756dcdb531148c8be2edf6
Alejandro Hernández Cano <alejandro.hernandezcano@epfl.ch>
2023-06-29 20:41:12 +0200

    fixed missing comma

--- megatron/arguments.py
+++ megatron/arguments.py
@@ -907,7 +907,7 @@ def _add_data_args(parser):
                        choices=['BertWordPieceLowerCase',
                                 'BertWordPieceCase',
                                 'GPT2BPETokenizer',
-                                'SentencePieceTokenizer']
+                                'SentencePieceTokenizer'],
                        help='What type of tokenizer to use.')
     group.add_argument('--tokenizer_model', type=str, default=None,
                        help='Sentencepiece tokenizer model.')

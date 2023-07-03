342e12d540f3d6f3cac096b80d29e1381cd6b1ee
Alejandro Hernández Cano <alejandro.hernandezcano@epfl.ch>
2023-07-03 20:43:26 +0200

    Fixed implementation of rotary embeddings

--- megatron/model/positional_embeddings.py
+++ megatron/model/positional_embeddings.py
@@ -25,9 +25,9 @@ def apply_rotary_emb(
     xk: torch.Tensor,
     freqs_cis: torch.Tensor,
 ) -> Tuple[torch.Tensor, torch.Tensor]:
-    xq_ = torch.view_as_complex(xq.float().reshape(*xq.shape[:-1], -1, 2))
-    xk_ = torch.view_as_complex(xk.float().reshape(*xk.shape[:-1], -1, 2))
+    xq_ = torch.view_as_complex(xq.float().reshape(*xq.shape[:-1], 2, -1).transpose(-1, -2).contiguous())
+    xk_ = torch.view_as_complex(xk.float().reshape(*xk.shape[:-1], 2, -1).transpose(-1, -2).contiguous())
     freqs_cis = reshape_for_broadcast(freqs_cis, xq_).to(xq.device)
-    xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(3)
-    xk_out = torch.view_as_real(xk_ * freqs_cis).flatten(3)
+    xq_out = torch.view_as_real(xq_ * freqs_cis).transpose(-1, -2).flatten(3)
+    xk_out = torch.view_as_real(xk_ * freqs_cis).transpose(-1, -2).flatten(3)
     return xq_out.type_as(xq), xk_out.type_as(xk)

db365686710cd7522e4bbd94274ab1f997302431
Alejandro Hernández Cano <alejandro.hernandezcano@epfl.ch>
2023-07-05 11:55:19 +0200

    updated and added example scripts to reproduce falcon pipeline progress

--- examples/verify_falcon.sh
+++ examples/verify_falcon.sh
@@ -1,46 +1,37 @@
 #! /bin/bash
 
-if [[ $# -ne 1 ]]; then
-	echo "Usage: $0 <node rank>"
-	exit 1
-fi
-
-GPUS_PER_NODE=4
+GPUS_PER_NODE=1
 NNODES=1
+RANK=0
 WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
-RANK=$1
 
 DATA_PATH=/scratch/wikitext-megatron/wikitext-train_text_document
 TENSORBOARD_PATH=/scratch/alhernan/megatron-data/tensorboard/
 
-
-## if random initialization:
-# CHECKPOINT_PATH=/scratch/alhernan/megatron-data/checkpoints/falcon-random/
-# NUM_LAYERS=8
-# HIDDEN_SIZE=8192
-# KV=8
-# NUM_HEADS=128
 ## if using falcon7:
-CHECKPOINT_PATH=/scratch/alhernan/megatron-data/checkpoints/falcon7b-tp1-pp4/
+CHECKPOINT_PATH=/scratch/alhernan/megatron-data/checkpoints/falcon7b/
 NUM_LAYERS=32
 HIDDEN_SIZE=4544
 KV=1
 NUM_HEADS=71
+SIZE=7
+MODEL=falcon
 ## if using falcon40:
-# CHECKPOINT_PATH=/scratch/alhernan/megatron-data/checkpoints/falcon40b-tp1-pp6/
+# CHECKPOINT_PATH=/scratch/alhernan/megatron-data/checkpoints/falcon40b/
 # NUM_LAYERS=60
 # HIDDEN_SIZE=8192
 # KV=8
 # NUM_HEADS=128
+# SIZE=40
+# MODEL=falcon
 
 
-DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $RANK --master_addr localhost --master_port 6000"
+DISTRIBUTED_ARGS="--nproc_per_node 1 --nnodes 1 --node_rank 0 --master_addr localhost --master_port 6000"
 
 
-# torchrun $DISTRIBUTED_ARGS pretrain_gpt.py \
-torchrun $DISTRIBUTED_ARGS finetune_falcon.py \
+torchrun $DISTRIBUTED_ARGS verify_correctness.py \
        --tensor_model_parallel_size 1 \
-       --pipeline_model_parallel_size 4 \
+       --pipeline_model_parallel_size 1 \
        --num_layers $NUM_LAYERS \
        --hidden_size $HIDDEN_SIZE \
        --use_flash_attn \
@@ -48,7 +39,7 @@ torchrun $DISTRIBUTED_ARGS finetune_falcon.py \
        --num_attention_heads_kv $KV \
        --num_attention_heads $NUM_HEADS \
        --micro_batch_size 1 \
-       --global_batch_size 8 \
+       --global_batch_size 16 \
        --seq_length 2048 \
        --max_position_embeddings 2048 \
        --train_iters 500000 \
@@ -66,7 +57,7 @@ torchrun $DISTRIBUTED_ARGS finetune_falcon.py \
        --clip_grad 1.0 \
        --lr_warmup_fraction .01 \
        --log_interval 100 \
-       --save_interval 1000 \
+       --save_interval 10000 \
        --eval_interval 1000 \
        --eval_iters 10 \
        --hidden_dropout 0.0 \
@@ -75,5 +66,6 @@ torchrun $DISTRIBUTED_ARGS finetune_falcon.py \
        --use_multiquery_attn \
        --parallel_attn \
        --no_bias_dropout_fusion \
-       --sequence_parallel \
-       --bf16
+       --huggingface_cache /scratch/alhernan/huggingface_cache/ \
+       --huggingface_device "cuda:4" \
+       --model_size $SIZE

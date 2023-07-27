99e9dc72134b2130a247a804a0a5f8112c8bd39d
Kyle Matoba <22180455+kylematoba@users.noreply.github.com>
2023-07-27 16:01:59 +0200

    add back in existing method

--- tests/test_multi_query_attn.py
+++ tests/test_multi_query_attn.py
@@ -29,6 +29,98 @@ logger.addHandler(standard_streamhandler)
 torch.manual_seed(111)
 
 
+
+class RotaryEmbedding(torch.nn.Module):
+    """
+    Implementation of RotaryEmbedding from GPT-NeoX.
+    This implementation is design to operate on queries and keys that are compatible with
+    [batch_size, n_heads_per_partition, seq_len, head_dim] (e.g. MinGPTAttention format).
+    """
+    def __init__(
+        self,
+        head_dim: int,
+        base=10000,
+    ):
+        super().__init__()
+        inv_freq = 1.0 / (base ** (torch.arange(0, head_dim, 2).float() / head_dim))
+        self.register_buffer("inv_freq", inv_freq, persistent=False)
+        self.head_dim = head_dim
+        self.seq_len_cached = None
+        self.batch_size_cached = None
+        self.cos_cached: torch.Tensor | None = None
+        self.sin_cached: torch.Tensor | None = None
+
+    def cos_sin(
+        self,
+        seq_len: int,
+        device="cuda",
+        dtype=torch.bfloat16,
+    ) -> torch.Tensor:
+        if seq_len != self.seq_len_cached:
+            self.seq_len_cached = seq_len
+            t = torch.arange(seq_len, device=device).type_as(self.inv_freq)
+            freqs = torch.einsum("i,j->ij", t, self.inv_freq)
+            emb = torch.cat((freqs, freqs), dim=-1).to(device)
+
+            if dtype in [torch.float16, torch.bfloat16]:
+                emb = emb.float()
+
+            self.cos_cached = emb.cos()[None, :, :]
+            self.sin_cached = emb.sin()[None, :, :]
+
+            self.cos_cached = self.cos_cached.type(dtype)
+            self.sin_cached = self.sin_cached.type(dtype)
+        return self.cos_cached, self.sin_cached
+
+    def forward(self, q, k):
+        batch, seq_len, head_dim = q.shape
+        cos, sin = self.cos_sin(seq_len, q.device, q.dtype)
+        return (q * cos) + (rotate_half(q) * sin), (k * cos) + (rotate_half(k) * sin)
+
+
+def _make_causal_mask(
+    input_ids_shape: torch.Size, device: torch.device, past_key_values_length: int
+) -> torch.BoolTensor:
+    batch_size, target_length = input_ids_shape
+    mask = torch.empty((target_length, target_length + past_key_values_length), dtype=torch.bool, device=device)
+    # ONNX doesn't support `torch.Tensor.triu` properly, thus we use this workaround
+    seq_ids = torch.arange(target_length, device=device)
+    mask[:, past_key_values_length:] = seq_ids[:, None] < seq_ids[None, :]
+
+    if past_key_values_length > 0:
+        mask[:, :past_key_values_length] = False
+
+    expanded_mask = mask[None, None, :, :].expand(batch_size, 1, target_length, target_length + past_key_values_length)
+    return expanded_mask
+
+
+def _expand_mask(mask: torch.Tensor, tgt_length: int) -> torch.BoolTensor:
+    batch_size, src_length = mask.shape
+    tgt_length = tgt_length if tgt_length is not None else src_length
+
+    expanded_mask = ~(mask[:, None, None, :].to(torch.bool))
+    return expanded_mask.expand(batch_size, 1, tgt_length, src_length)
+
+
+def rotate_half(x):
+    x1, x2 = x[..., : x.shape[-1] // 2], x[..., x.shape[-1] // 2:]
+    return torch.cat((-x2, x1), dim=x1.ndim - 1)  # dim=-1 triggers a bug in torch < 1.8.0
+
+
+def method1(batch_size: int,
+            q: torch.Tensor,
+            k: torch.Tensor):
+    nb, seq_length, head_dim = q.shape
+    num_head = nb // batch_size
+    falcon_rotary = RotaryEmbedding(head_dim)
+
+    q_falcon, k_falcon = falcon_rotary(q, k)
+    pattern = "batch num_heads seq_len head_dim -> seq_len batch num_heads head_dim"
+    q_out = rearrange(q_falcon.view(batch_size, num_head, seq_length, head_dim), pattern)
+    k_out = rearrange(k_falcon.view(batch_size, num_head, seq_length, head_dim), pattern)
+    return q_out, k_out
+
+
 def _make_causal_mask(
     input_ids_shape: torch.Size, device: torch.device, past_key_values_length: int
 ) -> torch.BoolTensor:
@@ -329,8 +421,8 @@ def benchmark_rotary_embeddings():
     k = torch.randn(nb, seq_length, conf.head_dim, device=device)
 
     # methods = [method2, method3]
-    methods = [method2, method3]
-    # methods = [method1, method3, method2]
+    # methods = [method2, method3]
+    methods = [method1, method3, method2]
     # methods = [method1, method2, method3]
     # methods = [method1, method3, method2, method4]
     # methods = list(reversed([method1, method2, method3]))

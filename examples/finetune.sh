0e2a5b17ced7db4fc341caf34ac07714a7ab55ab
Alejandro Hernández Cano <alejandro.hernandezcano@epfl.ch>
2023-08-20 14:00:15 +0200

    fixed finetune.sh + more options

--- examples/finetune.sh
+++ examples/finetune.sh
@@ -18,10 +18,11 @@ DATA=none
 WANDB_PROJ=none
 WANDB_ID=none
 ITERS=1000
+SEQ_LEN=none
 HELP_STR="[--rank=$RANK] [--size=$SIZE] [--tp=$TP] [--pp=$PP] [--gpus=$GPUS_PER_NODE] \
 [--micro-batch=$MICRO_BATCH] [--global-batch=$GLOBAL_BATCH] [--nodes=$N_NODES] \
 [--addr=$ADDR] [--wandb] [--instruct] [--checkpoint=...] [--data=...] [--iters=$ITERS] \
-[--wandb-proj=none] [--wandb-id=none] [--help]"
+[--wandb-proj=none] [--wandb-id=none] [--seq-len=...] [--help]"
 
 
 # define help function
@@ -55,10 +56,13 @@ while [[ $# -gt 0 ]]; do
 		--nodes) N_NODES=$2; shift; shift;;
 		--addr) ADDR=$2; shift; shift;;
 		--wandb) WANDB=1; shift;;
+		--wandb-project) WANDB_PROJ=$2; shift; shift;;
+		--wandb-id) WANDB_ID=$2; shift; shift;;
 		--instruct) INSTRUCT=1; shift;;
 		--checkpoint) CHECKPOINT_PATH=$2; shift; shift;;
-		--data) DATA=$2; shift; shift;;
+		--data) DATA_PATH=$2; shift; shift;;
 		--iters) ITERS=$2; shift; shift;;
+		--seq-len) SEQ_LEN=$2; shift; shift;;
 		*) echo unknown argument $1; help; exit 1;;
 	esac
 done
@@ -69,7 +73,7 @@ if [[ $CHECKPOINT_PATH = none ]]; then
 	CHECKPOINT_PATH=/pure-mlo-scratch/alhernan/megatron-data/checkpoints/${MODEL}-${SIZE}b-tp$TP-pp$PP
 fi
 
-if [[ $INSTRUCT = 1]]; then
+if [[ $INSTRUCT = 1 ]]; then
 	LR="2e-5"
 	TRAINED_PATH=$CHECKPOINT_PATH-pretrained
 else
@@ -82,25 +86,27 @@ DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $N_NODES --node_rank
                   $RANK --master_addr $ADDR --master_port 6000"
 
 if [[ $MODEL = falcon ]]; then
-	if [[ $DATA_PATH = none ]];
+	if [[ $DATA_PATH = none ]]; then
 		DATA_PATH=/pure-mlo-scratch/pagliard/data/wikitext-falcon/wiki-train_text_document
 	fi
 	TOKENIZER=FalconTokenizer
 	EXTRA_ARGS="--parallel_attn"
-	SEQ_LEN=2048
+	if [[ $SEQ_LEN = none ]]; then
+		SEQ_LEN=2048
+	fi
 elif [[ $MODEL = llama ]] || [[ $MODEL = llama2 ]]; then
 	# closing " missing intentionally
 	EXTRA_IDS='"[bib_ref],[/bib_ref],[fig_ref],[/fig_ref],[bib],[/bib],[fig],[/fig],[table],[/table],[formula],[/formula]'
 	EXTRA_ARGS="--vocab_file=/pure-mlo-scratch/llama/tokenizer.model --use_rms_norm
 	            --glu_activation swiglu --no_tie_embed_logits"
 	if [[ $INSTRUCT = 1 ]]; then
-		if [[ $DATA_PATH = none ]];
+		if [[ $DATA_PATH = none ]]; then
 			DATA_PATH=/pure-mlo-scratch/alhernan/data/orca/orca
 		fi
 		EXTRA_IDS="$EXTRA_IDS,<|im_start|>,<|im_end|>\""
        		EXTRA_ARGS="$EXTRA_ARGS --data_type instruction"
 	else
-		if [[ $DATA_PATH = none ]];
+		if [[ $DATA_PATH = none ]]; then
 			DATA_PATH=/pure-mlo-scratch/data/tokenized/pubmed-all/pubmed-all-llama_text_document
 		fi
 		EXTRA_IDS="$EXTRA_IDS\""
@@ -108,22 +114,29 @@ elif [[ $MODEL = llama ]] || [[ $MODEL = llama2 ]]; then
 	TOKENIZER=SentencePieceTokenizer
 	EXTRA_ARGS="$EXTRA_ARGS --vocab_extra_ids_list $EXTRA_IDS"
 	if [[ $MODEL == llama ]]; then
-		SEQ_LEN=2048
+		if [[ $SEQ_LEN = none ]]; then
+			SEQ_LEN=2048
+		fi
 		EXTRA_ARGS="$EXTRA_ARGS --layernorm_epsilon 1e-6"
 	else  # llama 2
-		SEQ_LEN=4096
+		if [[ $SEQ_LEN = none ]]; then
+			SEQ_LEN=4096
+		fi
+		EXTRA_ARGS="$EXTRA_ARGS --layernorm_epsilon 1e-6"
 		EXTRA_ARGS="$EXTRA_ARGS --layernorm_epsilon 1e-5"
 		if (( $SIZE > 13 )); then  # llama 2, 34B and 70B
 			LR="1.5e-4"
 		fi
 	fi
 elif [[ $MODEL = gpt ]]; then
-	if [[ $DATA_PATH = none ]];
+	if [[ $DATA_PATH = none ]]; then
 		DATA_PATH=/scratch/wikitext-megatron/wikitext-train_text_document
 	fi
 	TOKENIZER=FalconTokenizer
 	EXTRA_ARGS="--num_layers 4 --hidden_size 512 --num_attention_heads 8"
-	SEQ_LEN=2048
+	if [[ $SEQ_LEN = none ]]; then
+		SEQ_LEN=2048
+	fi
 else
 	echo "Model should be either gpt, llama or falcon, not $MODEL"
 	help
@@ -131,12 +144,16 @@ else
 fi
 COMMON_ARGS="--use_flash_attn --no_bias_gelu_fusion
 	     --seq_length $SEQ_LEN --max_position_embeddings $SEQ_LEN
-             --log_interval 1 --save_interval 50 --eval_interval 50
+             --log_interval 1 --save_interval 200 --eval_interval 200
              --eval_iters 10 --hidden_dropout 0.0 --position_embedding_type rotary
 	     --no_bias_dropout_fusion --use_checkpoint_args --train_iters $ITERS
 	     --attention_dropout 0.0 --adam_beta1 0.9 --adam_beta2 0.95 --adam_eps 1e-5
-	     --lr_decay_style cosine --lr_warmup_iters 0.1 --lr $LR --min_lr 1e-6
+	     --lr_decay_style cosine --lr_warmup_fraction 0.1 --lr $LR --min_lr 1e-6
 	     --weight_decay 0.1 --sequence_parallel --recompute_granularity selective"
+
+if [[ $INSTRUCT = 1 ]]; then
+	COMMON_ARGS="$COMMON_ARGS --finetune"
+fi
 
 if [[ $WANDB = 1 ]]; then
 	COMMON_ARGS="$COMMON_ARGS --wandb_logger"

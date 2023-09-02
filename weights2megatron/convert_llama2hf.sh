64d4c21051d3bb566fdbf1c6069e6b537c52eab5
Alejandro Hernández Cano <alejandro.hernandezcano@epfl.ch>
2023-09-02 03:30:41 +0200

    removed convert_llama2hf, use either decapoda-research/llama-7b-hf or meta-llama/Llama-2-7b-hf to download huggingface llama weights

--- weights2megatron/convert_llama2hf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
-NUM_SHARDS=8
-
-python3 /pure-mlo-scratch/sfan/model-parallel-trainer/llama2megatron/convert_llama2hf.py \
-    --input_dir /pure-mlo-scratch/llama/ --model_size 7B --output_dir /pure-mlo-scratch/llama/converted_HF_7B_8shard --num_output_shards $NUM_SHARDS
